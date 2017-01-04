d4e64a07b668b40a9728d8229f0e0209275506d2
realityone <realityone@me.com>
2017-01-04 14:44:26 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup
+
+ROOT_DIR = os.path.dirname(__file__)
+SOURCE_DIR = os.path.join(ROOT_DIR)
+
+install_requires = []
+
+setup(
+    name="dce-plugin-sdk",
+    version='0.1',
+    description="DCE plugin SDK for python.",
+    url='https://github.com/DaoCloud/dce-plugin-sdk-py',
+    packages=['dce_plugin'],
+    install_requires=install_requires,
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+    ],
+)
